396e16994b643cc17bea233292cff68f20a38015
lukaszplk <lpolowski@protonmail.com>
2026-01-31 16:57:30 +0100

    Bump version to 1.0.1 - Fix TypeError in start_game()

--- tetris_game/__init__.py
+++ tetris_game/__init__.py
@@ -4,7 +4,7 @@ Tetris Game Package
 A classic Tetris game implementation in Python using Pygame.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "lukaszplk"
 
 from .tetris import start_game as main
